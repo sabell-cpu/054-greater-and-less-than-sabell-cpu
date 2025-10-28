bbfc52f2f857c2d40f5b34b5b304192acd004cc1
sabell-cpu <sabell@anamosa.k12.ia.us>
2025-10-28 18:02:14 +0000

    greater-then-less-then

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+function oldEnough(age) {
+    if (age > 17) {
+        return "old enough";
+    }
+}
 
+function underAge(age) {
+    if (age < 18) {
+        return "under age";
+    }
+}
+
+function legalSpeed(speed) {
+    if (speed <= 50) {
+        return "legal speed";
+    }
+}
+
+function isGoldenYears(age) {
+    if (age >= 65) {
+        return "golden years";
+    }
+}
